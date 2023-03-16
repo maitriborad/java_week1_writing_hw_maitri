b836e7b8856615c8fb20c8efeb602476b93f6ddf
maitriborad <boradmaitrim@gmail.com>
2023-03-16 17:38:15 +0000

    My writing practice homework Maitri

---
+++ src/homework/Programme2_Printface.java
@@ -0,0 +1,13 @@
+package homework;
+/**
+ * 2. Write a Java program to print a face.
+ */
+public class Programme2_Printface {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  | ");
+        System.out.println(" | '_' | ");
+        System.out.println(" +_____+");
+    }
+}
